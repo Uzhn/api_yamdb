102a5f720dfd6efcf53b62e3d23d2fba194d83cc
PaulSar <paulssssar@gmail.com>
2023-01-31 14:34:38 +0700

    models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -27,20 +27,22 @@ class Titles(models.Model):
     category = models.ForeignKey(
         Categories,
         on_delete=models.SET_NULL,
+        null=True,
         verbose_name='Произведение',
         related_name='titles',
     )
     name = models.CharField(
+        max_length=256,
         unique=True
     )
     year = models.IntegerField(
         verbose_name='Год создания'
     )
     description = models.TextField
-
-    def __str__(self):
-        return self.name
     genre = models.ManyToManyField(
         Genre,
         verbose_name='Жанр'
     )
+
+    def __str__(self):
+        return self.name
