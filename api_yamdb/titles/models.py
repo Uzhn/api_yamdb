f0651c35de88b1e54c02c9cbd53dd9e974bf3705
PaulSar <paulssssar@gmail.com>
2023-01-31 11:54:51 +0700

    models

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -1,18 +1,21 @@
 from django.db import models
 
 
+class Categories(models.Model):
+    category = models.CharField(
+        unique=True
+    )
+    slug = models.SlugField(
+        unique=True
+    )
+
+
 class Titles(models.Model):
     category = models.ForeignKey(
         Categories,
-        on_delete = models.SET_NULL,
+        on_delete=models.SET_NULL,
         verbose_name='Произведение',
         related_name='titles',
-    )
-    genre = models.ForeignKey(
-        Genre,
-        on_delete = models.SET_NULL,
-        verbose_name='Жанр',
-        related_name='title_genre',
     )
     name = models.CharField(
         unique=True
@@ -23,3 +26,18 @@ class Titles(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Genre(models.Model):
+    genre = models.CharField(
+        unique=True
+    )
+    slug = models.SlugField(
+        unique=True
+    )
+    title = models.ForeignKey(
+        Titles,
+        on_delete=models.SET_NULL,
+        verbose_name='Жанр',
+        related_name='genre',
+    )
