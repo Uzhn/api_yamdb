8d1803894d807b89e25a8dfb4bf7d1195b6301a4
Евгений Попов <evgeniirg73@gmail.com>
2023-01-31 19:26:07 +0700

    model User

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -3,6 +3,19 @@ from django.db import models
 
 
 class User(AbstractUser):
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+
+    ROLE_CHOICES = [
+        (USER, 'user'),
+        (MODERATOR, 'moderator'),
+        (ADMIN, 'admin')
+    ]
+
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(max_length=254, unique=True)
-    bio = models.TextField('Биография', blank=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    bio = models.TextField(verbose_name='Биография', blank=True)
+    role = models.CharField(max_length=9, choices=ROLE_CHOICES, default=USER)
