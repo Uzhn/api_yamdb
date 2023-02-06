8e58832924d9c7664f4c881c584fa7c52655b995
Саввинов Павел <pavel-savvinov0@mail.ru>
2023-02-06 22:11:29 +0900

    Добавлен permission для title, genre, categories

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -35,9 +35,3 @@ class Other(permissions.BasePermission):
                 or (request.user.is_authenticated
                     and request.user.is_superuser)
                 )
-
-    def has_object_permission(self, request, view, obj):
-        return ((request.method in permissions.SAFE_METHODS)
-                or request.user.is_admin
-                or request.user.is_superuser
-                )
