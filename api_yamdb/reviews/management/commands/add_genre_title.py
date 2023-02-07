cbec4185ca5d1af148f8b49542123de11fb66f42
Евгений Попов <evgeniirg73@gmail.com>
2023-02-07 20:31:50 +0700

    first

--- api_yamdb/reviews/management/commands/add_genre_title.py
+++ api_yamdb/reviews/management/commands/add_genre_title.py
@@ -2,13 +2,15 @@ import csv
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
+
 from reviews.models import Genre, Title
-from users.models import User
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        with open(f'{settings.BASE_DIR}/static/data/genre_title.csv', newline='', encoding='utf-8') as file:
+        with open(f'{settings.BASE_DIR}/static/data/genre_title.csv',
+                  newline='', encoding='utf-8'
+                  ) as file:
             reader = csv.reader(file)
             count = 0
             for row in reader:
@@ -16,5 +18,5 @@ class Command(BaseCommand):
                     genre = Genre.objects.get(id=row[2])
                     title = Title.objects.get(id=row[1])
                     genre.title_set.add(title)
-                count+=1
+                count += 1
             self.stdout.write(self.style.SUCCESS('Данные загружены в БД'))
