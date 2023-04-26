6eda5e8d6c7bfb9a20022eec8dd071c5aa04b3b1
Pavel Sarygin <paulsssar@yandex.ru>
2023-04-26 10:20:55 +0700

    docker

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -111,9 +111,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static'
+#STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
